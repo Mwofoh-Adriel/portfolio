215706dcbd93844cf82ea00429798c64f2ad9b9a
Sergius Nyah <sergiusnyah@gmail.com>
2024-04-11 09:43:08 +0100

    Implement readers count

--- client/js/Writing.js
+++ client/js/Writing.js
@@ -3,3 +3,15 @@ let options = { month: 'long', day: 'numeric', year: 'numeric' };
 let completeDate = date.toLocaleDateString('en-US', options);
 
 document.getElementById('publish-date').textContent = completeDate;
+
+let duration = "5 min";
+document.getElementById('duration').textContent = duration;
+
+let clickCount = 0;
+let blogTitle = document.getElementById('blog-title');
+
+blogTitle.addEventListener('click', function() {
+    clickCount++;
+    document.getElementById('num-reads').textContent = clickCount;
+}
+);
