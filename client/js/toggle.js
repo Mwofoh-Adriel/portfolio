f7675df06893c2aa5ee07da8e512259e0d33ca1a
Sergius Nyah <sergiusnyah@gmail.com>
2024-04-12 15:45:04 +0100

    Remove Local storage

--- client/js/toggle.js
+++ client/js/toggle.js
@@ -1,17 +1,3 @@
-var savedTheme = localStorage.getItem('theme');
-if(savedTheme){
-    document.body.className = savedTheme;
-    var icon = document.querySelector('#theme-switcher i');
-    if(savedTheme === 'light-mode'){
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
-    }
-    else{
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-    }
-}
-
 document.getElementById('theme-switcher').addEventListener('click', function() {
     var body = document.body;
     var icon = document.querySelector('#theme-switcher i');
@@ -27,17 +13,12 @@ document.getElementById('theme-switcher').addEventListener('click', function() {
         icon.classList.remove('fa-sun');
         icon.classList.add('fa-moon');
     }
-
-    // Save the theme to local storage
-    localStorage.setItem('theme', body.className);
 });
+
 window.onload = function(){
     var body = document.body;
     var dot = document.getElementsByClassName('dot');
     dot[0].classList.add('visible');
-    if(!localStorage.getItem('theme')){
-        body.classList.add('light-mode');
-    }
+    body.classList.add('light-mode');
 };
-
 
